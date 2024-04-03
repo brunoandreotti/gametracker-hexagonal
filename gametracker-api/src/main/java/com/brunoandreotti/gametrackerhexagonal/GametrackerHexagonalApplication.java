a196f5228f202022a97139b7d11ae1f29ae41948
Bruno <brunoandreotti@outlook.com>
2024-04-03 13:59:31 -0300

    estrutura inicial

---
+++ gametracker-api/src/main/java/com/brunoandreotti/gametrackerhexagonal/GametrackerHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.brunoandreotti.gametrackerhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametrackerHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametrackerHexagonalApplication.class, args);
+	}
+
+}
